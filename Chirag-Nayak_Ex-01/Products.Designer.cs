81a4b819ec60ff50cd1b6d69db2c042e19e4480e
Chirag1805Nayak <200493655@student.georgianc.on.ca>
2022-10-26 17:57:23 -0400

    Add project files.

---
+++ Chirag-Nayak_Ex-01/Products.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Chirag_Nayak_Ex_01
+{
+    partial class Products
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.compradioButton = new System.Windows.Forms.RadioButton();
+            this.elecradioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.deliveryradioButton = new System.Windows.Forms.RadioButton();
+            this.pickupradioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.listrichTextBox = new System.Windows.Forms.RichTextBox();
+            this.listOfProductsComboBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.clearProductsButton = new System.Windows.Forms.Button();
+            this.summarylistBox = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.phoneTextBox);
+            this.groupBox1.Controls.Add(this.emailTextBox);
+            this.groupBox1.Controls.Add(this.addressTextBox);
+            this.groupBox1.Controls.Add(this.nameTextBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(286, 220);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Customer Information";
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(121, 188);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(100, 22);
+            this.phoneTextBox.TabIndex = 7;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(121, 142);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(100, 22);
+            this.emailTextBox.TabIndex = 6;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(121, 97);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(100, 22);
+            this.addressTextBox.TabIndex = 5;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(121, 52);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 22);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Phone:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.compradioButton);
+            this.groupBox2.Controls.Add(this.elecradioButton);
+            this.groupBox2.Location = new System.Drawing.Point(304, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(227, 68);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Product Category";
+            // 
+            // compradioButton
+            // 
+            this.compradioButton.AutoSize = true;
+            this.compradioButton.Location = new System.Drawing.Point(128, 35);
+            this.compradioButton.Name = "compradioButton";
+            this.compradioButton.Size = new System.Drawing.Size(93, 20);
+            this.compradioButton.TabIndex = 1;
+            this.compradioButton.TabStop = true;
+            this.compradioButton.Text = "Computers";
+            this.compradioButton.UseVisualStyleBackColor = true;
+            // 
+            // elecradioButton
+            // 
+            this.elecradioButton.AutoSize = true;
+            this.elecradioButton.Location = new System.Drawing.Point(12, 35);
+            this.elecradioButton.Name = "elecradioButton";
+            this.elecradioButton.Size = new System.Drawing.Size(94, 20);
+            this.elecradioButton.TabIndex = 0;
+            this.elecradioButton.TabStop = true;
+            this.elecradioButton.Text = "Electronics";
+            this.elecradioButton.UseVisualStyleBackColor = true;
+            this.elecradioButton.CheckedChanged += new System.EventHandler(this.elecradioButton_CheckedChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.deliveryradioButton);
+            this.groupBox3.Controls.Add(this.pickupradioButton);
+            this.groupBox3.Location = new System.Drawing.Point(537, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 68);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Delivery Option";
+            // 
+            // deliveryradioButton
+            // 
+            this.deliveryradioButton.AutoSize = true;
+            this.deliveryradioButton.Location = new System.Drawing.Point(108, 34);
+            this.deliveryradioButton.Name = "deliveryradioButton";
+            this.deliveryradioButton.Size = new System.Drawing.Size(78, 20);
+            this.deliveryradioButton.TabIndex = 1;
+            this.deliveryradioButton.TabStop = true;
+            this.deliveryradioButton.Text = "Delivery";
+            this.deliveryradioButton.UseVisualStyleBackColor = true;
+            // 
+            // pickupradioButton
+            // 
+            this.pickupradioButton.AutoSize = true;
+            this.pickupradioButton.Location = new System.Drawing.Point(1, 35);
+            this.pickupradioButton.Name = "pickupradioButton";
+            this.pickupradioButton.Size = new System.Drawing.Size(76, 20);
+            this.pickupradioButton.TabIndex = 0;
+            this.pickupradioButton.TabStop = true;
+            this.pickupradioButton.Text = "Pick-Up";
+            this.pickupradioButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.listrichTextBox);
+            this.groupBox4.Controls.Add(this.listOfProductsComboBox);
+            this.groupBox4.Location = new System.Drawing.Point(304, 86);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(433, 146);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "List of Products";
+            // 
+            // listrichTextBox
+            // 
+            this.listrichTextBox.Location = new System.Drawing.Point(155, 19);
+            this.listrichTextBox.Name = "listrichTextBox";
+            this.listrichTextBox.Size = new System.Drawing.Size(264, 111);
+            this.listrichTextBox.TabIndex = 2;
+            this.listrichTextBox.Text = "";
+            // 
+            // listOfProductsComboBox
+            // 
+            this.listOfProductsComboBox.FormattingEnabled = true;
+            this.listOfProductsComboBox.Location = new System.Drawing.Point(7, 22);
+            this.listOfProductsComboBox.Name = "listOfProductsComboBox";
+            this.listOfProductsComboBox.Size = new System.Drawing.Size(121, 24);
+            this.listOfProductsComboBox.TabIndex = 1;
+            this.listOfProductsComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(82, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Display";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // clearProductsButton
+            // 
+            this.clearProductsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearProductsButton.Location = new System.Drawing.Point(606, 238);
+            this.clearProductsButton.Name = "clearProductsButton";
+            this.clearProductsButton.Size = new System.Drawing.Size(131, 36);
+            this.clearProductsButton.TabIndex = 5;
+            this.clearProductsButton.Text = "Clear Products";
+            this.clearProductsButton.UseVisualStyleBackColor = false;
+            this.clearProductsButton.Click += new System.EventHandler(this.clearProductsButton_Click);
+            // 
+            // summarylistBox
+            // 
+            this.summarylistBox.FormattingEnabled = true;
+            this.summarylistBox.ItemHeight = 16;
+            this.summarylistBox.Location = new System.Drawing.Point(6, 280);
+            this.summarylistBox.Name = "summarylistBox";
+            this.summarylistBox.Size = new System.Drawing.Size(791, 164);
+            this.summarylistBox.TabIndex = 6;
+            // 
+            // Products
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.summarylistBox);
+            this.Controls.Add(this.clearProductsButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Products";
+            this.Text = "Products Order Form ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton compradioButton;
+        private System.Windows.Forms.RadioButton elecradioButton;
+        private System.Windows.Forms.RadioButton deliveryradioButton;
+        private System.Windows.Forms.RadioButton pickupradioButton;
+        private System.Windows.Forms.Button clearProductsButton;
+        private System.Windows.Forms.ComboBox listOfProductsComboBox;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.RichTextBox listrichTextBox;
+        private System.Windows.Forms.ListBox summarylistBox;
+    }
+}
+
---
+++ Chirag-Nayak_Ex-01/Products.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chirag_Nayak_Ex_01
+{
+    public partial class Products : Form
+    {
+        string Message = "";
+        string Message2 = "";
+        string Message3 = "";
+        public Products()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Message3 += listOfProductsComboBox.SelectedItem.ToString();
+            listrichTextBox.Text = Message3;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (nameTextBox.Text == string.Empty)
+            {
+                MessageBox.Show("Name Can not be Empty", "Errors", MessageBoxButtons.OK);
+            }
+            else if(addressTextBox.Text == string.Empty)
+            {
+                MessageBox.Show("Address Can not be Empty", "Errors", MessageBoxButtons.OK);
+            }
+            else if (emailTextBox.Text == string.Empty)
+            {
+                MessageBox.Show("Email Can not be Empty", "Errors", MessageBoxButtons.OK);
+            }
+            else if (phoneTextBox.Text == string.Empty)
+            {
+                MessageBox.Show("Phone Number Can not be Empty", "Errors", MessageBoxButtons.OK);
+            }
+            else if (listrichTextBox.Text == string.Empty)
+            {
+                MessageBox.Show("List of Products Can not be empty", "Errors", MessageBoxButtons.OK);
+            }
+
+            if (elecradioButton.Checked)
+            {
+                Message += elecradioButton.Text;
+            }
+            else if (compradioButton.Checked)
+            {
+                Message += compradioButton.Text;
+            } 
+            if (pickupradioButton.Checked)
+            {
+                Message2 += pickupradioButton.Text; 
+            }
+            else if (deliveryradioButton.Checked)
+            {
+                Message2 += deliveryradioButton.Text;
+            }
+            
+            summarylistBox.Items.Add(nameTextBox.Text +"   "+ emailTextBox.Text +"   "+Message + "  -  " + Message2 +"  -  "+Message3);  
+
+
+            nameTextBox.Clear();
+            addressTextBox.Clear();
+            emailTextBox.Clear();
+            phoneTextBox.Clear();
+            compradioButton.Checked = false;
+            elecradioButton.Checked = false;
+            pickupradioButton.Checked=false;
+            deliveryradioButton.Checked=false;
+        }
+
+        private void clearProductsButton_Click(object sender, EventArgs e)
+        {
+            compradioButton.Checked = false;
+            elecradioButton.Checked = false;
+            pickupradioButton.Checked = false;
+            deliveryradioButton.Checked = false;
+            listrichTextBox.Clear();
+            listOfProductsComboBox.Items.Clear();   
+        }
+
+        private void elecradioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if(elecradioButton.Checked == true)
+            {
+                listOfProductsComboBox.Items.Clear();
+                listOfProductsComboBox.Items.Add("TVs");
+                listOfProductsComboBox.Items.Add("Video Games");
+                listOfProductsComboBox.Items.Add("Digital Cameras");
+                listOfProductsComboBox.Items.Add("Home Audio");
+            }
+            if (compradioButton.Checked == true)
+            {
+                listOfProductsComboBox.Items.Clear();
+                listOfProductsComboBox.Items.Add("Desktops");
+                listOfProductsComboBox.Items.Add("Laptops");
+                listOfProductsComboBox.Items.Add("Monitors");
+                listOfProductsComboBox.Items.Add("Printers");
+            }
+        }
+
+       
+    }
+}
